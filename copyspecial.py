191452bcdb1a89b8a56bf77ec3935780e7bf2541
James Maupin <jmsMaupinRC@gmail.com>
2019-11-20 12:04:39 -0500

    Add ability to get absolute path of special files and ability to copy give files to a given dir

--- copyspecial.py
+++ copyspecial.py
@@ -16,11 +16,26 @@ import subprocess
 import argparse
 
 # This is to help coaches and graders identify student assignments
-__author__ = "???"
+__author__ = "jmsMaupin1"
 
+def get_absolute_paths(directory):
+    """Checks a directory to see if any files match our regex, return them if so"""
+    files = os.listdir(directory)
+    regex = re.compile(r'__\w*__')
 
-# +++your code here+++
-# Write functions and modify main() to call them
+    return [os.path.abspath(f) for f in files if regex.search(f)]
+
+def copy_files_to(files, directory):
+    """Copies files to a given directory using shutil"""
+    if not os.path.isdir(directory):
+        try:
+            os.makedirs(directory)
+        except OSError:
+            print("Creation of directory %s failed" % directory)
+            return 1
+
+    for f in files:
+        shutil.copyfile(f, directory + "/" + files.split("/")[-1])      
 
 def main():
     # This snippet will help you get started with the argparse module.
@@ -29,6 +44,8 @@ def main():
     parser.add_argument('--tozip', help='dest zipfile for special files')
     # TODO need an argument to pick up 'from_dir'
     args = parser.parse_args()
+    # print(get_absolute_paths("./"))
+    # copy_files_to(get_absolute_paths("./"), "./tmp")
 
     # TODO you must write your own code to get the cmdline args.
     # Read the docs and examples for the argparse module about how to do this.
